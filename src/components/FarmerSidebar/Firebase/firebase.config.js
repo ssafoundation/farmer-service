c19a3749492059dc832808f20669db8255a4b1c4
Akhtaruzzaman <akhtar01947@gmail.com>
2021-04-19 18:57:49 +0600

    update

--- src/components/FarmerSidebar/Firebase/firebase.config.js
+++ src/components/FarmerSidebar/Firebase/firebase.config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-   apiKey: "AIzaSyAsWf6q1SIb5S3BYeID20FlMTN4x9CFjO8",
-   authDomain: "farmers-services.firebaseapp.com",
-   projectId: "farmers-services",
-   storageBucket: "farmers-services.appspot.com",
-   messagingSenderId: "814986536141",
-   appId: "1:814986536141:web:376a05547b7c495fdb7584"
+   apiKey: "AIzaSyDmDlJbn9mB2pMEYTSxEt6ionEGIRRYxAw",
+   authDomain: "asm-farmer.firebaseapp.com",
+   projectId: "asm-farmer",
+   storageBucket: "asm-farmer.appspot.com",
+   messagingSenderId: "251346508850",
+   appId: "1:251346508850:web:7267829374f122be12b02b"
 };
